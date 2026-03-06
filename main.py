492f93da16cba843a48ea45ef6cdfb1a7970289c
zinwutyeezaw <111890101+Dorothy00007@users.noreply.github.com>
2026-03-06 08:52:26 +0630

    Update main.py

--- main.py
+++ main.py
@@ -205,7 +205,7 @@ elif page == "🔍 Data Explorer":
 # ============================================
 # ML Models Page
 # ============================================
-elif page == "🤖 ML Models":
+elif page == "🤖 Feature Importance":
     st.markdown("<h1 class='main-header'>🤖 ML Models</h1>", unsafe_allow_html=True)
     
     features = ['StudyTimeWeekly', 'Absences', 'ParentalSupport', 'TotalActivities']
